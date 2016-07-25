0dcc05dcb16e11027df0606f7a2138205ff3709c
cosmos-it <cosmostaban@gmail.com>
2016-07-25 09:35:10 -0700

    Updates

---
+++ DataStructure/Cities.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructure
+ {
+    public class Cities
+    {
+         public string CityName {get; set;}
+         public long CityPopulation {get; set;}
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -2,27 +2,83 @@ using System;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
-
 using System.Threading.Tasks;
 
-namespace MasteringC_
+namespace DataStructure
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            System.Collections.Generic.List<String> list = new List<String>();
-            list.Add("Test 1");
-            list.Add("Test 2");
+            var cities = CityData();
+            string line;
+            do
+            {
+                Console.WriteLine("Enter value for search:  ");
+                line = Console.ReadLine();
+                if (line != null)
+                    for (var i = 0; i < cities.Count; i++)
+                    {
+                        if (cities[i].CityName.Contains(line))
+                        {
+                            Console.WriteLine("Found: {0}, {1}", cities[i].CityName, cities[i].CityPopulation);
+                        }
 
-            for (var i=0; i <list.Count; i++){
-                    Console.WriteLine("List are: {0}", list[i]);
-            }
+                    }
+
+            } while (line != null);
+
+        }
+
+        //Cities data
+        public static List<Cities> CityData()
+        {
+            var seattle = new Cities();
+            List<Cities> cities = new List<Cities>();
+
+            seattle.CityName = "Seattle WA";
+            seattle.CityPopulation = 652405;
+
+            var redmond = new Cities();
+            redmond.CityName = "Redmond WA";
+            redmond.CityPopulation = 57530;
 
 
-            
+            var spokane = new Cities();
+            spokane.CityName = "Spokane WA";
+            spokane.CityPopulation = 208916;
+
+            var bellevue = new Cities();
+            bellevue.CityName = "Bellevue MO";
+            bellevue.CityPopulation = 200123;
+
+            var everette = new Cities();
+            everette.CityName = "Everette WA";
+            everette.CityPopulation = 133992;
+
+            var olympia = new Cities();
+            olympia.CityName = "Olympia WA";
+            olympia.CityPopulation = 48338;
+
+            var tacoma = new Cities();
+            tacoma.CityName = "Bellevue WA";
+            tacoma.CityPopulation = 133992;
+
+            var ellensburge = new Cities();
+            ellensburge.CityName = "Ellensburge WA";
+            ellensburge.CityPopulation = 18111;
+
+            cities.Add(seattle);
+            cities.Add(redmond);
+            cities.Add(spokane);
+            cities.Add(bellevue);
+            cities.Add(everette);
+            cities.Add(olympia);
+            cities.Add(tacoma);
+            cities.Add(ellensburge);
 
 
+            return cities;
         }
     }
 }
